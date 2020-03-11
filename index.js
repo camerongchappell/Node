3f6f7a3512909aeabe380dd1dcad62ab02828915
Cameron Chappell <cameron.g.chappell@gmail.com>
2020-03-10 18:33:37 -0600

    Adding route for Assignment09

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
+const app = express();
+
+const w09Router = require('./Assignments/Week 09/postalService');
+
+app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+  .use(express.urlencoded({ extended: true}))
+  .use(express.json())
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .use('/postalService', w09Router);
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
