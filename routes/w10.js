0d1d12b0f9cdfdb94d97d1b3c652445bf4f053c4
Cameron Chappell <cameron.g.chappell@gmail.com>
2020-03-31 14:20:22 -0600

    Editing suitDetails endpoint

--- routes/w10.js
+++ routes/w10.js
@@ -3,7 +3,7 @@ const app = express();
 var router = express.Router();
 
 router.get("/getSuit", getSuit);
-router.get('/suitDetails', function(req, res, next) {
+router.get('/w10/suitDetails', function(req, res, next) {
 	  res.render('pages/suitDetails.ejs');
 	});
 
